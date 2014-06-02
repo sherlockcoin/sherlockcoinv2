c54fb6406db0ac7bf7c14aaddf11fd15da19f217
sherlockcoin <sherlockcoin@gmail.com>
2014-06-02 11:36:59 +0530

    Update main.cpp
    Pool Mining Fix

--- src/main.cpp
+++ src/main.cpp
@@ -1655,18 +1655,6 @@ bool CBlock::ConnectBlock(CTxDB& txdb, CBlockIndex* pindex, bool fJustCheck)
                    vtx[0].GetValueOut(),
                    nReward));
     }
-
-    if(IsProofOfWork())
-    {
-        CBitcoinAddress address(!fTestNet ? FOUNDATION : FOUNDATION_TEST);
-        CScript scriptPubKey;
-        scriptPubKey.SetDestination(address.Get());
-        if (vtx[0].vout[1].scriptPubKey != scriptPubKey)
-            return error("ConnectBlock() : coinbase does not pay to the dev address)");
-        if (vtx[0].vout[1].nValue < devCoin)
-            return error("ConnectBlock() : coinbase does not pay enough to dev addresss");
-    }
-
     if (IsProofOfStake())
     {
         // sherlockcoinv2: coin stake tx earns reward instead of paying fee
